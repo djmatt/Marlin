96ca1e38d52b374c4935833fd14be674faa4bb7a
Matthew Dallmeyer <d01matt@gmail.com>
2018-03-04 13:28:59 -0500

    Measured and calibrated the extruder steps, esteps value updated.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -529,7 +529,7 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 396, 160 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 396, 153 }
 
 /**
  * Default Max Feed Rate (mm/s)
